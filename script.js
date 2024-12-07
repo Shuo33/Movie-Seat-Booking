f9e1989b3c903ed242cb0647ad1e691833397c29
shuo xu <shuo.xu@2021.icam.fr>
2024-12-07 17:38:21 +0100

    save data to local storage

--- script.js
+++ script.js
@@ -9,19 +9,40 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+// save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+
+
 // update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount; 
     total.innerText = selectedSeatsCount * ticketPrice;
+
+    // document.querySelectorAll() gives a nodelist, we convert the nodelist into a regular array so we can map through it, since map() gives an array. and here we got the array with the index of the selected seats
+    const seatsIndex = [...selectedSeats].map(function (seat) {
+        return [...seats].indexOf(seat);
+    });
+
+    //save the selected seats to localStorage
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 }
     
 
 // movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value; 
+
+    // save the selected one to localStorage
+    setMovieData(e.target.selectedIndex, e.target.value);
+
     updateSelectedCount();
 });
 
